eed8ea910626c8b7e8f2ecc73c9893edfbfc2ae9
oguzhanalayont <oguzhanalayont2021@gmail.com>
2025-07-07 17:43:47 +0300

    logo edited

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -137,7 +137,7 @@ export default function HomeScreen() {
       <ScrollView
   showsVerticalScrollIndicator={false}
   contentContainerStyle={{
-    paddingTop: 48,
+    paddingTop: 44,
     paddingBottom: 0,
   }}
       >
@@ -240,7 +240,7 @@ const styles = StyleSheet.create({
   },
   logo: {
     width: 100,
-    height: 70,
+    height: 60,
     resizeMode: 'contain',
   },
 
